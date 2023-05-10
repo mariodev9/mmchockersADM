0c88bd46615e47ef4a3cc6b51933cc44b6027a9a
LucianoMariotti <luciano.mariotti99@gmail.com>
2023-05-10 14:53:36 -0300

    fix

---
+++ pages/gastos/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Layout from "../../components/layout";
+
+export default function GastosPage() {
+  return (
+    <Layout>
+      <h1>gastos</h1>
+    </Layout>
+  );
+}
