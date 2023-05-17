4ffe9e292eb96ba3db6cbc49df9566c0658ed97b
LucianoMariotti <luciano.mariotti99@gmail.com>
2023-05-17 03:20:24 -0300

    fix security in all pages

--- pages/ventas/[id].js
+++ pages/ventas/[id].js
@@ -13,6 +13,8 @@ import {
   StatusShippingBox,
 } from "../../components/common/Table/SingleRowSell";
 import Image from "next/image";
+import useUser from "../../hooks/useUser";
+import PageSpinner from "../../components/common/PageSpinner/PageSpinner";
 
 const Wraper = ({ children }) => (
   <Box bg={"#fff"} w={"full"} p={"15px"} borderRadius={"15px"}>
@@ -28,6 +30,8 @@ const WraperTitle = ({ children }) => (
 
 export default function SingleSellPage() {
   const router = useRouter();
+  const user = useUser();
+
   const { query } = router;
   const { id } = query;
 
@@ -40,86 +44,101 @@ export default function SingleSellPage() {
   }, [id]);
 
   return (
-    <Layout>
-      <Text fontSize={"20px"}>Detalles de compra</Text>
-      <Box mt={"20px"}>
-        {saleData ? (
-          <VStack align={"center"} spacing={5}>
-            <Text w={"full"} align={"start"}>
-              ID de compra: {query?.id} <br />
-              ID corto: {query.id?.slice(0, 6)} <br />
-            </Text>
-            <Wraper>
-              <WraperTitle>Informacion del comprador</WraperTitle>
+    <>
+      {user ? (
+        <Layout>
+          <Text fontSize={"20px"}>Detalles de compra</Text>
+          <Box mt={"20px"}>
+            {saleData ? (
+              <VStack align={"center"} spacing={5}>
+                <Text w={"full"} align={"start"}>
+                  ID de compra: {query?.id} <br />
+                  ID corto: {query.id?.slice(0, 6)} <br />
+                </Text>
+                <Wraper>
+                  <WraperTitle>Informacion del comprador</WraperTitle>
 
-              <Text>
-                Nombre completo: {saleData.buyerData?.name}{" "}
-                {saleData.buyerData.lastName} <br />
-                DNI: {saleData.buyerData.dni} <br />
-                E-mail: {saleData.buyerData.email} <br />
-                Destino: {saleData.buyerData.adress}{" "}
-                {saleData.buyerData.apartment &&
-                  `apartamento: ${saleData.buyerData.apartment}`}
-                {saleData.buyerData.adressNumber}, {saleData.buyerData.city}
-                {", "}
-                {saleData.buyerData.province} <br />
-                Codigo Postal: {saleData.buyerData.postalCode}
-              </Text>
-            </Wraper>
-            <Wraper>
-              <WraperTitle>Productos comprados</WraperTitle>
-              <Flex direction={"column"}>
-                {saleData.cart.map((product) => (
-                  <Flex key={product.id} justify={"space-between"} mt={"10px"}>
-                    <Box width={"70px"} height={"70px"} pos={"relative"}>
-                      <Image
-                        src={product.images[0]}
-                        alt={`image of ${product.name}`}
-                        style={{ borderRadius: "2px" }}
-                        layout="fill"
-                        objectFit="cover"
-                      />
-                    </Box>
-                    <Flex
-                      width={"50%"}
-                      direction={"column"}
-                      justify={"space-between"}
-                    >
-                      <Text fontSize={"14px"} fontWeight={600}>
-                        {product.name}
-                      </Text>
-                      <Text fontSize={"14px"} fontWeight={600}>
-                        cantidad: {product.quantity}
-                      </Text>
-                    </Flex>
-                    <Flex align={"end"} fontWeight={600}>
-                      ${product.price}
-                    </Flex>
+                  <Text>
+                    Nombre completo: {saleData.buyerData?.name}{" "}
+                    {saleData.buyerData.lastName} <br />
+                    DNI: {saleData.buyerData.dni} <br />
+                    E-mail: {saleData.buyerData.email} <br />
+                    Destino: {saleData.buyerData.adress}{" "}
+                    {saleData.buyerData.apartment &&
+                      `apartamento: ${saleData.buyerData.apartment}`}
+                    {saleData.buyerData.adressNumber}, {saleData.buyerData.city}
+                    {", "}
+                    {saleData.buyerData.province} <br />
+                    Codigo Postal: {saleData.buyerData.postalCode}
+                  </Text>
+                </Wraper>
+                <Wraper>
+                  <WraperTitle>Productos comprados</WraperTitle>
+                  <Flex direction={"column"}>
+                    {saleData.cart.map((product) => (
+                      <Flex
+                        key={product.id}
+                        justify={"space-between"}
+                        mt={"10px"}
+                      >
+                        <Box width={"70px"} height={"70px"} pos={"relative"}>
+                          <Image
+                            src={product.images[0]}
+                            alt={`image of ${product.name}`}
+                            style={{ borderRadius: "2px" }}
+                            layout="fill"
+                            objectFit="cover"
+                          />
+                        </Box>
+                        <Flex
+                          width={"50%"}
+                          direction={"column"}
+                          justify={"space-between"}
+                        >
+                          <Text fontSize={"14px"} fontWeight={600}>
+                            {product.name}
+                          </Text>
+                          <Text fontSize={"14px"} fontWeight={600}>
+                            cantidad: {product.quantity}
+                          </Text>
+                        </Flex>
+                        <Flex align={"end"} fontWeight={600}>
+                          ${product.price}
+                        </Flex>
+                      </Flex>
+                    ))}
                   </Flex>
-                ))}
-              </Flex>
-            </Wraper>
+                </Wraper>
 
-            <Wraper>
-              <WraperTitle>Informacion de compra</WraperTitle>
-              <Text mb={"5px"}>Total a pagar: $ {saleData.totalPayment}</Text>
-              <Flex gap={5}>
-                <Text>Estado del envio:</Text>
-                <StatusShippingBox
-                  saleId={id}
-                  status={saleData.shippingStatus}
-                />
-              </Flex>
-              <Flex gap={5} mt={"5px"}>
-                <Text>Estado de compra:</Text>
-                <StatusPaymentBox saleId={id} status={saleData.paymentStatus} />
-              </Flex>
-            </Wraper>
-          </VStack>
-        ) : (
-          <Spinner />
-        )}
-      </Box>
-    </Layout>
+                <Wraper>
+                  <WraperTitle>Informacion de compra</WraperTitle>
+                  <Text mb={"5px"}>
+                    Total a pagar: $ {saleData.totalPayment}
+                  </Text>
+                  <Flex gap={5}>
+                    <Text>Estado del envio:</Text>
+                    <StatusShippingBox
+                      saleId={id}
+                      status={saleData.shippingStatus}
+                    />
+                  </Flex>
+                  <Flex gap={5} mt={"5px"}>
+                    <Text>Estado de compra:</Text>
+                    <StatusPaymentBox
+                      saleId={id}
+                      status={saleData.paymentStatus}
+                    />
+                  </Flex>
+                </Wraper>
+              </VStack>
+            ) : (
+              <Spinner />
+            )}
+          </Box>
+        </Layout>
+      ) : (
+        <PageSpinner />
+      )}
+    </>
   );
 }
